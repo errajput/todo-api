13867fae7dfcd35e2e5af8d011a12b6b4348cd6c
disha rajput <dishacsevira@gmail.com>
2025-10-02 16:54:38 +0530

    add DB name from env

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import AuthRoutes from "./routes/authRoutes.js";
 import TodosRoutes from "./routes/todosRoutes.js";
 
 const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = process.env.DB_NAME || "todo-api";
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-mongoose.connect(MONGODB_URI, { dbName: "product-curd" });
 mongoose.connection.on("connected", () => console.log("MongoDB Connected"));
+mongoose.connection.on("error", (e) => console.log("MongoDB Error: ", e));
+mongoose.connect(MONGODB_URI, { dbName: DB_NAME });
 
 app.use(express.json());
 app.use(cors());
